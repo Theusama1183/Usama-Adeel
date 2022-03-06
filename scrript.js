5e2900c3b623d1bb98d9d8f15ec3d746f8a3ee1a
Theusama1183 <87718937+Theusama1183@users.noreply.github.com>
2022-03-06 19:54:14 +0500

    Add files via upload

---
+++ scrript.js
@@ -0,0 +1,58 @@
+const maremove = document.getElementById('maremove')
+
+// OnScroll event handler
+const onScroll = () => {
+
+  // Get scroll value
+  const scroll = document.documentElement.scrollTop
+
+  // If scroll value is more than 0 - add class
+  if (scroll > 0) {
+    maremove.classList.add("sticky-column");
+  } else {
+    maremove.classList.remove("sticky-column")
+  }
+}
+// Use the function
+window.addEventListener('scroll', onScroll)
+
+
+
+
+// Get the portfolio modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
